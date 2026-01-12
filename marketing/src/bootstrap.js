f1494d0b1b20a4de36a02c4b956f52133c7081bc
yb9255 <yoobin9255@gmail.com>
2026-01-13 01:19:33 +0900

    marketing app에 첫 리액트 연결

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// "mount" function to start up the app
+
+const mount = (el) => {
+  ReactDOM.render(<h1>Hi there!</h1>, el);
+};
+
+// If we are in development in isolation
+// call mount immediately
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.getElementById('_marketing-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// We are running through container
+// and we should export the mount function
+export { mount };
