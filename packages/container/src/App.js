43e8b338a32759b0b779bb66bb0d33ff137d510f
yb9255 <yoobin9255@gmail.com>
2026-03-08 02:17:36 +0900

    Header 컴포넌트 이동
    Signed-off-by: yb9255 <yoobin9255@gmail.com>

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
-import Header from '../components/Header';
+import Header from './components/Header';
 import { StylesProvider, createGenerateClassName } from '@material-ui/styles';
 
 const generateClassName = createGenerateClassName({
